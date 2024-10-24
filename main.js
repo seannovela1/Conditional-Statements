3725a8ef1cea2cc647c9d3b02bbba48337c4720b
seannovela1 <seannovela5484@my.whccd.edu>
2024-10-23 18:54:26 -0700

    committed modified "mygrade"

--- main.js
+++ main.js
@@ -14,3 +14,23 @@ if (myGrade >= 90) {
 } else {
     console.log("F");
 }
+// Modify the value inside of “myGrade” to test your code
+myGrade = 72; // Change this value to test different scenarios
+
+// Write a switch statement that does the same thing as the if/else-if/else statement above
+switch (true) {
+    case (myGrade >= 90):
+        console.log("A");
+        break;
+    case (myGrade >= 80):
+        console.log("B");
+        break;
+    case (myGrade >= 70):
+        console.log("C");
+        break;
+    case (myGrade >= 60):
+        console.log("D");
+        break;
+    default:
+        console.log("F");
+}
