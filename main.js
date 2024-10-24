5ebefff5c39fbc53ee99c86f77ae37cafeb42f98
seannovela1 <seannovela5484@my.whccd.edu>
2024-10-23 18:52:29 -0700

    committed javascript

---
+++ main.js
@@ -0,0 +1,16 @@
+javascript
+// Declare a new variable “myGrade” and assign a number value from 0-100 to it.
+let myGrade = 85; // You can modify this value to test your code
+
+// Write an if/else-if/else statement that displays the letter grade using console.log(...)
+if (myGrade >= 90) {
+    console.log("A");
+} else if (myGrade >= 80) {
+    console.log("B");
+} else if (myGrade >= 70) {
+    console.log("C");
+} else if (myGrade >= 60) {
+    console.log("D");
+} else {
+    console.log("F");
+}
